af9abb95f386cbbd70c4d90738f3e9af7d2e8419
Emre Can Özdemir <93479988+ozdemiremrecan@users.noreply.github.com>
2023-01-26 23:01:54 +0300

    Create EmreCoin.sol

---
+++ EmreCoin.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract EmreCoin is ERC20, ERC20Burnable, Ownable {
+    constructor() ERC20("EmreCoin", "EMR") {}
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+    function burnFrom(address account, uint256 amount) public override {
+        _burn(account, amount);
+    }
+}
